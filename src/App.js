b919655c274456f74705f6df8e1cc766ab6d8665
OahidZihad <oahidzihad1@gmail.com>
2021-06-24 03:35:00 +0600

    Implement Facebook authentication

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import "firebase/firestore";
 import firebaseConfig from "./firebase.config";
 import { useState } from "react";
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 
 function App() {
   const [user, setUser] = useState({});
-  var provider = new firebase.auth.GoogleAuthProvider();
+  var googleProvider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
   const handleGoogleSignIn = () => {
     firebase
       .auth()
-      .signInWithPopup(provider)
+      .signInWithPopup(googleProvider)
       .then((result) => {
         /** @type {firebase.auth.OAuthCredential} */
         var credential = result.credential;
@@ -38,10 +43,42 @@ function App() {
         // ...
       });
   };
+  const handleFbSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // The signed-in user info.
+        var user = result.user;
+        console.log("Successfully logged in", user);
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var accessToken = credential.accessToken;
+
+        setUser(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, credential, email);
+
+        // ...
+      });
+  };
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Sign In Using Google</button>
-      <h3>Email: {user.email}</h3>
+      <br /> <br />
+      <button onClick={handleFbSignIn}>Sign In Using Facebook</button>
+      <h3>User: {user.displayName}</h3>
       <img src={user.photoURL} alt="" />
     </div>
   );
